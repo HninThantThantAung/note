11e3100001dd21b98af6237af66cca7c74e2c3b4
HninThantThantAung <hninthantthantaung123@gmail.com>
2023-05-23 15:11:21 +0630

    second commit

--- src/app/allin-one.service.ts
+++ src/app/allin-one.service.ts
@@ -13,15 +13,15 @@ export class AllinOneService {
     let length = this.note.length;
     console.log("note array", this.note);
     
-    let data: any = localStorage.getItem('note');
-    console.log("data",data);
+    let notedata: any = localStorage.getItem('note');
+    console.log("data",notedata);
     
     // console.log("get data >>", JSON.parse('data'));
     // console.log("length:", this.note.length);
     
     // this.note[0] = JSON.parse('{"title": "reminder", "body":"11AM the body11AM the body11AM the body11AM the body11AM the body11AM the body"}');
     // this.note[1] = JSON.parse('{"title": "alarm", "body":"12AM"}');
-    this.note[length]= JSON.parse(data);
+    this.note[length]= JSON.parse(notedata);
     console.log("Json format of data ", this.note);
     
     return this.note;
